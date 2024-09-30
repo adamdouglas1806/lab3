2f5581d518ddfc30b81f108025613be6a13f3b24
Kayvan Karim <k1karim@gmail.com>
2024-09-30 12:31:57 +0400

    Init

---
+++ src/MathQuiz.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class MathQuiz {
+
+	public static void main(String[] args) {
+		// TODO Write your code here. You can use the sample random number generator. 
+		
+		/*
+		 * Here is a random generator example. 
+		 * This code returns a pseudorandom, uniformly distributed int value 
+		 * between 0 (inclusive) and the specified value (exclusive).
+		 */
+		Random random = new Random();
+		System.out.print(random.nextInt(100));
+
+
+	}
+
+}
